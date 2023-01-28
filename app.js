f4cbee0170ca55eb9d73696316eca85974d06f49
weipo <b10901053@ntu.edu.tw>
2023-01-29 02:44:55 +0800

    delete

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ const Blog = require('./models/blog');
 const app = express();
 
 // connect to mongodb & listen for requests
-const dbURI = "mongodb+srv://netninja:test1234@net-ninja-tuts-del96.mongodb.net/node-tuts";
+const dbURI = 'mongodb+srv://weipo:sa20021207@cluster0.v5z3cgh.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(3000))
+  .then(result => {
+    app.listen(3000)
+    console.log("connect to db")
+  })
   .catch(err => console.log(err));
 
 // register view engine
@@ -18,7 +21,7 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); //essential! a middleware takes all the url data pass into the data we can request 
 app.use(morgan('dev'));
 app.use((req, res, next) => {
   res.locals.path = req.path;
@@ -39,7 +42,7 @@ app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
 });
 
-app.get('/blogs', (req, res) => {
+app.get('/blogs', (req, res) => { //把所有blog用出來
   Blog.find().sort({ createdAt: -1 })
     .then(result => {
       res.render('index', { blogs: result, title: 'All blogs' });
@@ -49,11 +52,11 @@ app.get('/blogs', (req, res) => {
     });
 });
 
-app.post('/blogs', (req, res) => {
-  // console.log(req.body);
+app.post('/blogs', (req, res) => {//post的位置要跟create.ejs裡的相同
+  //console.log(req.body.body);
   const blog = new Blog(req.body);
 
-  blog.save()
+  blog.save() 
     .then(result => {
       res.redirect('/blogs');
     })
@@ -64,6 +67,7 @@ app.post('/blogs', (req, res) => {
 
 app.get('/blogs/:id', (req, res) => {
   const id = req.params.id;
+  //console.log(id)
   Blog.findById(id)
     .then(result => {
       res.render('details', { blog: result, title: 'Blog Details' });
@@ -76,9 +80,9 @@ app.get('/blogs/:id', (req, res) => {
 app.delete('/blogs/:id', (req, res) => {
   const id = req.params.id;
   
-  Blog.findByIdAndDelete(id)
+  Blog.findByIdAndDelete(id) //delete from the database
     .then(result => {
-      res.json({ redirect: '/blogs' });
+      res.json({ redirect: '/blogs' }); //delete sth 
     })
     .catch(err => {
       console.log(err);
