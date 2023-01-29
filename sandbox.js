ebbda3a9b09797af756ba675bca8c43cae211e89
weiber2002 <b10901053@ntu.edu.tw>
2023-01-30 02:44:47 +0800

    readable

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,17 @@
-fetch('todo/todo.json').then((res)=>{
-    console.log('resolved',res)
-    return res.json(); //get the res data and  & return promise
-}).then(data=>{
-    console.log(data)
-}).catch(err=>{
-    console.log(err)
-});
+//async & await so the code can carry on
+const getTodo = async()=>{ //async will return promise
+    const response = await fetch('todo/todo.json')  //await stop from assigning the value until fetch is done
+    const data = await response.json();
+    return data;
+}
+getTodo(). //得到一個promise
+    then(data=>{console.log(data)}).catch(err=>console.log(err))
+
+// fetch('todo/todo.json').then((res)=>{
+//     console.log('resolved',res)
+//     return res.json(); //get the res data and  & return promise
+// }).then(data=>{
+//     console.log(data)
+// }).catch(err=>{
+//     console.log(err)
+// });
